e151ab6bfe8c7735a29090e47e269258403f36ad
Andrei Zhlobich <andrei.zhlobich@appodeal.com>
2023-05-06 17:00:34 +0200

    add project.clj

---
+++ project.clj
@@ -0,0 +1,8 @@
+(defproject knitty "0.1.0-SNAPSHOT"
+  :description "FIXME: write description"
+  :url "http://example.com/FIXME"
+  :license {:name "EPL-2.0", :url "https://www.eclipse.org/legal/epl-2.0/"}
+  :dependencies [[org.clojure/clojure "1.11.1"]
+                 [manifold "0.4.0"]
+                 [macroz/tangle "0.2.2"]]
+  :repl-options {:init-ns ag.knitty.core})
