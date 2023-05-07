b372be6449bf936491a191d780b682a01cc69475
Andrei Zhlobich <andrei.zhlobich@appodeal.com>
2023-05-07 12:51:38 +0200

    Version 0.1.0

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject knitty "0.1.0-SNAPSHOT"
+(defproject knitty "0.1.0"
   :description "FIXME: write description"
   :url "http://example.com/FIXME"
   :license {:name "EPL-2.0", :url "https://www.eclipse.org/legal/epl-2.0/"}
