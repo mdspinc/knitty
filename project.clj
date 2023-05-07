d56a34729f21f93ccc706ffcbe71c3424d18f139
Andrei Zhlobich <andrei.zhlobich@appodeal.com>
2023-05-07 13:50:48 +0200

    add LICENSE

--- project.clj
+++ project.clj
@@ -1,7 +1,7 @@
 (defproject knitty "0.1.0-SNAPSHOT"
   :description "FIXME: write description"
   :url "http://example.com/FIXME"
-  :license {:name "EPL-2.0", :url "https://www.eclipse.org/legal/epl-2.0/"}
+  :license {:name "MIT License" :url "http://opensource.org/licenses/MIT"}
   :dependencies [[org.clojure/clojure "1.11.1"]
                  [manifold "0.4.0"]
                  [macroz/tangle "0.2.2"]]
