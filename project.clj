32fe55b9d472c3fb47170af104054e6fab93bacb
Andrei Zhlobich <andrei.zhlobich@appodeal.com>
2023-05-12 21:44:57 +0200

    add hdr-histogram dep

--- project.clj
+++ project.clj
@@ -3,7 +3,8 @@
   :url "http://example.com/FIXME"
   :license {:name "EPL-2.0", :url "https://www.eclipse.org/legal/epl-2.0/"}
   :dependencies [[org.clojure/clojure "1.11.1"]
-                 [manifold "0.4.0"]
+                 [manifold/manifold "0.2.4"]
+                 [org.hdrhistogram/HdrHistogram "2.1.12"]
                  [macroz/tangle "0.2.2"]]
   :repl-options {:init-ns ag.knitty.core}
   :plugins [[io.github.borkdude/lein-lein2deps "0.1.0"]]
