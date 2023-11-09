03c7e1da705d21bc8282931b12cb9b9366a491e9
Andrei Zhlobich <andrei.zhlobich@appodeal.com>
2023-11-10 00:30:25 +0100

    refactor mdm.clj

--- src/main/clojure/ag/knitty/mdm.clj
+++ src/main/clojure/ag/knitty/mdm.clj
@@ -1,37 +1,34 @@
 (ns ag.knitty.mdm
-  (:require [ag.knitty.deferred :as kd]
-            [manifold.deferred :as md])
-  (:import [clojure.lang Associative]
-           [ag.knitty MDM]
-           [java.util.concurrent ConcurrentHashMap]
-           [java.util.concurrent.atomic AtomicReference AtomicReferenceArray]))
-
+  (:import [ag.knitty MDM]))
 
 (set! *warn-on-reflection* true)
 (set! *unchecked-math* :warn-on-boxed)
 
-(definline max-initd [] `(MDM/maxid))
-(definline keyword->intid [k] `(MDM/regkw ~k))
+(definline max-initd []
+  `(MDM/maxid))
 
-(defmacro mdm-fetch! [mdm kid]
+(definline keyword->intid [k]
+  `(MDM/regkw ~k))
+
+(definline mdm-fetch! [mdm kid]
   (list '.fetch (with-meta mdm {:tag "ag.knitty.MDM"}) kid))
 
-(defmacro mdm-freeze! [mdm]
+(definline mdm-freeze! [mdm]
   (list '.freeze (with-meta mdm {:tag "ag.knitty.MDM"})))
 
-(defmacro mdm-cancel! [mdm]
+(definline mdm-cancel! [mdm]
   (list '.cancel (with-meta mdm {:tag "ag.knitty.MDM"})))
 
-(defmacro mdm-get! [mdm kid]
+(definline mdm-get! [mdm kid]
   (list '.get (with-meta mdm {:tag "ag.knitty.MDM"}) kid))
 
-(defmacro fetch-result-claimed? [r]
+(definline fetch-result-claimed? [r]
   (list '.-claimed (with-meta r {:tag "ag.knitty.MDM$Result"})))
 
-(defmacro fetch-result-value [r]
+(definline fetch-result-value [r]
   (list '.-value (with-meta r {:tag "ag.knitty.MDM$Result"})))
 
-(defmacro none? [x]
+(definline none? [x]
   `(MDM/isNone ~x))
 
 (defn create-mdm [init]
