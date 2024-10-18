7de4b0aac9b69ab0afd0a1d0edfaa9eb0bb9a36b
Andrei Zhlobich <andrei.zhlobich@appodeal.com>
2024-10-18 15:25:20 +0200

    cherrypick tests from manifold

---
+++ test/knitty/manifold/deferred_test.clj
@@ -0,0 +1,444 @@
+#_>>>(ns knitty.manifold.deferred-test
+#_>>>   {:clj-kondo/ignore true}
+;;<<<(ns manifold.deferred-test
+  (:refer-clojure :exclude (realized? future loop))
+  (:require
+   [clojure.test :refer :all]
+;;<<<   [manifold.test-utils :refer :all]
+;;<<<   [manifold.deferred :as d]
+;;<<<   [manifold.executor :as ex])
+#_>>>   [knitty.test-util :refer :all]
+#_>>>   [knitty.manifold.fake-md :as d])
+  (:import
+   (java.util.concurrent
+    CompletableFuture
+    CompletionStage)
+   (manifold.deferred IDeferred)))
+
+#_>>> (set! *warn-on-reflection* false)
+#_>>> (clojure.test/use-fixtures :each (clojure.test/join-fixtures
+#_>>>                                    [(d/fixup-promesa-print-method-test)
+#_>>>                                     (d/fixup-count-loggeg-errors)]))
+#_>>> (clojure.test/use-fixtures :once (d/with-modes-fixture))
+
+(defmacro future' [& body]
+  `(d/future
+     (Thread/sleep 10)
+     (try
+       ~@body
+       (catch Exception e#
+         (.printStackTrace e#)))))
+
+(defn future-error
+  [ex]
+  (d/future
+    (Thread/sleep 10)
+    (throw ex)))
+
+(defn capture-success
+  ([result]
+   (capture-success result true))
+  ([result expected-return-value]
+   (let [p (promise)]
+     (d/on-realized result
+                    #(do (deliver p %) expected-return-value)
+                    (fn [_] (throw (Exception. "ERROR"))))
+     p)))
+
+(defn capture-error
+  ([result]
+   (capture-error result true))
+  ([result expected-return-value]
+   (let [p (promise)]
+     (d/on-realized result
+                    (fn [_] (throw (Exception. "SUCCESS")))
+                    #(do (deliver p %) expected-return-value))
+     p)))
+
+(deftest test-catch
+  (is (thrown? ArithmeticException
+               @(-> 0
+                    (d/chain #(/ 1 %))
+                    (d/catch IllegalStateException (constantly :foo)))))
+
+  (is (thrown? ArithmeticException
+               @(-> 0
+                    d/future
+                    (d/chain #(/ 1 %))
+                    (d/catch IllegalStateException (constantly :foo)))))
+
+  (is (= :foo
+         @(-> 0
+              (d/chain #(/ 1 %))
+              (d/catch ArithmeticException (constantly :foo)))))
+
+  (let [d (d/deferred)]
+    (d/future (Thread/sleep 100) (d/error! d :bar))
+    (is (= :foo @(d/catch d (constantly :foo)))))
+
+  (is (= :foo
+         @(-> (d/error-deferred :bar)
+              (d/catch (constantly :foo)))))
+
+  (is (= :foo
+         @(-> 0
+              d/future
+              (d/chain #(/ 1 %))
+              (d/catch ArithmeticException (constantly :foo))))))
+
+#_>>> #_
+(def ^:dynamic *test-dynamic-var*)
+
+#_>>> #_
+(deftest test-let-flow
+
+  (let [flag (atom false)]
+    @(let [z (clojure.core/future 1)]
+       (d/let-flow [x (d/future (clojure.core/future z))
+                    _ (d/future (Thread/sleep 1000) (reset! flag true))
+                    y (d/future (+ z x))]
+                   (d/future (+ x x y z))))
+    (is (= true @flag)))
+
+  (is (= 5
+         @(let [z (clojure.core/future 1)]
+            (d/let-flow [x (d/future (clojure.core/future z))
+                         y (d/future (+ z x))]
+                        (d/future (+ x x y z))))))
+
+  (is (= 2
+         @(let [d (d/deferred)]
+            (d/let-flow [[x] (future' [1])]
+                        (d/let-flow [[x'] (future' [(inc x)])
+                                     y (future' true)]
+                                    (when y x'))))))
+  (testing "let-flow callbacks happen on different executor retain thread bindings"
+    (let [d (d/deferred (ex/fixed-thread-executor 1))
+          test-internal-fn (fn [] (let [x *test-dynamic-var*]
+                                    (d/future (Thread/sleep 100) (d/success! d x))))]
+      (binding [*test-dynamic-var* "cat"]
+        (test-internal-fn)
+        (is (= ["cat" "cat" "cat"]
+               @(d/let-flow [a d
+                             b (do a *test-dynamic-var*)]
+                            [a b *test-dynamic-var*])))))))
+
+(deftest test-chain-errors
+  (let [boom (fn [n] (throw (ex-info "" {:n n})))]
+    (doseq [b [boom (fn [n] (d/future (boom n)))]]
+      (dorun
+       (for [i (range 10)
+             j (range 10)]
+         (let [fs (concat (repeat i inc) [boom] (repeat j inc))]
+           (is (= i
+                  @(-> (apply d/chain 0 fs)
+                       (d/catch (fn [e] (:n (ex-data e)))))
+                  @(-> (apply d/chain' 0 fs)
+                       (d/catch' (fn [e] (:n (ex-data e)))))))))))))
+
+(deftest test-chain
+  (dorun
+   (for [i (range 10)
+         j (range i)]
+     (let [fs  (take i (cycle [inc #(* % 2)]))
+           fs' (-> fs
+                   vec
+                   (update-in [j] (fn [f] #(d/future (f %)))))]
+       (is
+        (= (reduce #(%2 %1) 0 fs)
+           @(apply d/chain 0 fs')
+           @(apply d/chain' 0 fs')))))))
+
+(deftest test-deferred
+  ;; success!
+  (let [d (d/deferred)]
+    (is (= true (d/success! d 1)))
+    (is (= 1 @(capture-success d)))
+    (is (= 1 @d)))
+
+  ;; claim and success!
+  (let [d     (d/deferred)
+        token (d/claim! d)]
+    (is token)
+    (is (= false (d/success! d 1)))
+    (is (= true (d/success! d 1 token)))
+    (is (= 1 @(capture-success d)))
+    (is (= 1 @d)))
+
+  ;; error!
+  (let [d  (d/deferred)
+        ex (IllegalStateException. "boom")]
+    (is (= true (d/error! d ex)))
+    (is (= ex @(capture-error d ::return)))
+    (is (thrown? IllegalStateException @d)))
+
+  ;; claim and error!
+  (let [d     (d/deferred)
+        ex    (IllegalStateException. "boom")
+        token (d/claim! d)]
+    (is token)
+    (is (= false (d/error! d ex)))
+    (is (= true (d/error! d ex token)))
+    (is (= ex @(capture-error d ::return)))
+    (is (thrown? IllegalStateException (deref d 1000 ::timeout))))
+
+  ;; test deref with delayed result
+  (let [d (d/deferred)]
+    (future' (d/success! d 1))
+    (is (= 1 (deref d 1000 ::timeout))))
+
+  ;; test deref with delayed error result
+  (let [d (d/deferred)]
+    (future' (d/error! d (IllegalStateException. "boom")))
+    (is (thrown? IllegalStateException (deref d 1000 ::timeout))))
+
+  ;; test deref with non-Throwable error result
+  (are [d timeout]
+       (= :bar
+          (-> (is (thrown? clojure.lang.ExceptionInfo
+                           (if timeout (deref d 1000 ::timeout) @d)))
+              ex-data
+              :error))
+
+    (doto (d/deferred) (d/error! :bar)) true
+
+    (doto (d/deferred) (as-> d (future' (d/error! d :bar)))) true
+
+    (d/error-deferred :bar) true
+
+    (d/error-deferred :bar) false)
+
+  ;; multiple callbacks w/ success
+  (let [n               50
+        d               (d/deferred)
+        callback-values (->> (range n)
+                             (map (fn [_] (d/future (capture-success d))))
+                             (map deref)
+                             doall)]
+    (is (= true (d/success! d 1)))
+    (is (= 1 (deref d 1000 ::timeout)))
+    (is (= (repeat n 1) (map deref callback-values))))
+
+  ;; multiple callbacks w/ error
+  (let [n               50
+        d               (d/deferred)
+        callback-values (->> (range n)
+                             (map (fn [_] (d/future (capture-error d))))
+                             (map deref)
+                             doall)
+        ex              (Exception.)]
+    (is (= true (d/error! d ex)))
+    (is (thrown? Exception (deref d 1000 ::timeout)))
+    (is (= (repeat n ex) (map deref callback-values))))
+
+  ;; cancel listeners
+  (let [l (d/listener (constantly :foo) nil)
+        d (d/deferred)]
+    (is (= false (d/cancel-listener! d l)))
+    (is (= true (d/add-listener! d l)))
+    (is (= true (d/cancel-listener! d l)))
+    (is (= true (d/success! d :foo)))
+    (is (= :foo @(capture-success d)))
+    (is (= false (d/cancel-listener! d l))))
+
+  ;; deref
+  (let [d (d/deferred)]
+    (is (= :foo (deref d 10 :foo)))
+    (d/success! d 1)
+    (is (= 1 @d))
+    (is (= 1 (deref d 10 :foo)))))
+
+(deftest test-loop
+  ;; body produces a non-deferred value
+  (is @(capture-success
+        (d/loop [] true)))
+
+  ;; body raises exception
+  (let [ex (Exception.)]
+    (is (= ex @(capture-error
+                (d/loop [] (throw ex))))))
+
+  ;; body produces a realized result
+  (is @(capture-success
+        (d/loop [] (d/success-deferred true))))
+
+  ;; body produces a realized error result
+  (let [ex (Exception.)]
+    (is (= ex @(capture-error
+                (d/loop [] (d/error-deferred ex))))))
+
+  ;; body produces a delayed result
+  (is @(capture-success
+        (d/loop [] (future' true))))
+
+  ;; body produces a delayed error result
+  (let [ex (Exception.)]
+    (is (= ex @(capture-error
+                (d/loop [] (future-error ex))))))
+
+  ;; destructuring works for loop parameters
+  (is (= 1 @(capture-success
+             (d/loop [{:keys [a]} {:a 1}] a))))
+  (is @(capture-success
+        (d/loop [[x & xs] [1 2 3]] (or (= x 3) (d/recur xs))))))
+
+(deftest test-coercion
+  (is (= 1 (-> 1 clojure.core/future d/->deferred deref)))
+
+  (let [f (CompletableFuture.)]
+    (.obtrudeValue f 1)
+    (is (= 1 (-> f d/->deferred deref))))
+
+  (let [f (CompletableFuture.)]
+    (.obtrudeException f (Exception.))
+    (is (thrown? Exception (-> f d/->deferred deref)))))
+
+(deftest test-finally
+  (let [target-d (d/deferred)
+        d        (d/deferred)
+        fd       (d/finally
+                   d
+                   (fn []
+                     (d/success! target-d ::delivered)))]
+    (d/error! d (Exception.))
+    (is (= ::delivered (deref target-d 0 ::not-delivered)))))
+
+(deftest test-alt
+  (is (#{1 2 3} @(d/alt 1 2 3)))
+  (is (= 2 @(d/alt (d/future (Thread/sleep 10) 1) 2)))
+
+  (is (= 2 @(d/alt (d/future (Thread/sleep 10) (throw (Exception. "boom"))) 2)))
+
+  (is (thrown-with-msg? Exception #"boom"
+                        @(d/alt (d/future (throw (Exception. "boom"))) (d/future (Thread/sleep 10)))))
+
+  (testing "uniformly distributed"
+    (let [results (atom {})
+          ;; within 10%
+          n       1e4, r 10, eps (* n 0.15)
+          f       #(/ (% n eps) r)]
+      (dotimes [_ n]
+        @(d/chain (apply d/alt (range r))
+                  #(swap! results update % (fnil inc 0))))
+      (doseq [[i times] @results]
+        (is (<= (f -) times (f +)))))))
+
+;;;
+
+(deftest ^:benchmark benchmark-chain
+  (bench "invoke comp x1"
+         ((comp inc) 0))
+  (bench "chain x1"
+         @(d/chain 0 inc))
+  (bench "chain' x1"
+         @(d/chain' 0 inc))
+  (bench "invoke comp x2"
+         ((comp inc inc) 0))
+  (bench "chain x2"
+         @(d/chain 0 inc inc))
+  (bench "chain' x2"
+         @(d/chain' 0 inc inc))
+  (bench "invoke comp x5"
+         ((comp inc inc inc inc inc) 0))
+  (bench "chain x5"
+         @(d/chain 0 inc inc inc inc inc))
+  (bench "chain' x5"
+         @(d/chain' 0 inc inc inc inc inc)))
+
+(deftest ^:benchmark benchmark-deferred
+  (bench "create deferred"
+         (d/deferred))
+  (bench "add-listener and success"
+         (let [d (d/deferred)]
+           (d/add-listener! d (d/listener (fn [_]) nil))
+           (d/success! d 1)))
+  (bench "add-listener, claim, and success!"
+         (let [d (d/deferred)]
+           (d/add-listener! d (d/listener (fn [_]) nil))
+           (d/success! d 1 (d/claim! d))))
+  (bench "add-listener!, cancel, add-listener! and success"
+         (let [d (d/deferred)]
+           (let [callback (d/listener (fn [_]) nil)]
+             (d/add-listener! d callback)
+             (d/cancel-listener! d callback))
+           (d/add-listener! d (d/listener (fn [_]) nil))
+           (d/success! d 1)))
+  (bench "multi-add-listener! and success"
+         (let [d (d/deferred)]
+           (d/add-listener! d (d/listener (fn [_]) nil))
+           (d/add-listener! d (d/listener (fn [_]) nil))
+           (d/success! d 1)))
+  (bench "multi-add-listener!, cancel, and success"
+         (let [d (d/deferred)]
+           (d/add-listener! d (d/listener (fn [_]) nil))
+           (let [callback (d/listener (fn [_]) nil)]
+             (d/add-listener! d callback)
+             (d/cancel-listener! d callback))
+           (d/success! d 1)))
+  (bench "success! and add-listener!"
+         (let [d (d/deferred)]
+           (d/success! d 1)
+           (d/add-listener! d (d/listener (fn [_]) nil))))
+  (bench "claim, success!, and add-listener!"
+         (let [d (d/deferred)]
+           (d/success! d 1 (d/claim! d))
+           (d/add-listener! d (d/listener (fn [_]) nil))))
+  (bench "success! and deref"
+         (let [d (d/deferred)]
+           (d/success! d 1)
+           @d))
+  (bench "deliver and deref"
+         (let [d (d/deferred)]
+           (deliver d 1)
+           @d)))
+
+(deftest ^:stress test-error-leak-detection
+
+  (d/error-deferred (Throwable.))
+  (System/gc)
+
+  (dotimes [_ 2e3]
+    (d/error! (d/deferred) (Throwable.)))
+  (System/gc))
+
+(deftest ^:stress test-deferred-chain
+  (dotimes [_ 1e4]
+    (let [d      (d/deferred)
+          result (d/future
+                   (last
+                    (take 1e4
+                          (iterate
+                           #(let [d' (d/deferred)]
+                              (d/connect % d')
+                              d')
+                           d))))]
+      (Thread/sleep (rand-int 10))
+      (d/success! d 1)
+      (is (= 1 @@result)))))
+
+;; Promesa adds CompletionStage to the print-method hierarchy, which can cause
+;; problems if neither is preferred over the other
+(deftest promesa-print-method-test
+  (testing "print-method hierarchy compatibility with promesa")
+  (try
+    (let [print-method-dispatch-vals (-> print-method methods keys set)]
+      (is (= IDeferred
+             (get print-method-dispatch-vals IDeferred ::missing)))
+      (is (= ::missing
+             (get print-method-dispatch-vals CompletionStage ::missing)))
+
+      (let [d (d/deferred)]
+        (is (instance? IDeferred d))
+        (is (instance? CompletionStage d))
+
+        (testing "no conflicts - CompletionStage not dispatchable"
+          (pr-str d))
+
+        (testing "no conflicts - preferred hierarchy established"
+          (defmethod print-method CompletionStage [o ^java.io.Writer w]
+            :noop)
+
+          (pr-str d))))
+
+    (finally
+      (remove-method print-method CompletionStage))))
---
+++ test/knitty/manifold/fake_md.clj
@@ -0,0 +1,200 @@
+(ns knitty.manifold.fake-md
+  (:refer-clojure :exclude [future future-call loop])
+  (:require [clojure.test :refer [testing]]
+            [clojure.tools.logging :as log]
+            [clojure.tools.logging.test :as log-test]
+            [knitty.deferred :as kd]
+            [manifold.deferred :as md])
+  (:import [clojure.lang IDeref]
+           [java.util.concurrent CompletionStage]))
+
+(defn chain [x & fs]
+  (kd/chain* x fs))
+
+(defn catch
+  ([x ef] (kd/bind-err x ef))
+  ([x ex ef] (kd/bind-err x ex ef)))
+
+(defn chain' [x & fs]
+  (kd/chain* x fs))
+
+(defn catch'
+  ([x ef] (kd/bind-err x ef))
+  ([x ex ef] (kd/bind-err x ex ef)))
+
+(defn finally [x f]
+  (kd/bind-fin x f))
+
+(defn ->deferred [x]
+  (kd/wrap* x))
+
+(defn connect [x y]
+  (kd/connect x y))
+
+(defn alt [& xs]
+  (apply kd/alt xs))
+
+(defn zip [& xs]
+  (apply kd/zip xs))
+
+(defn on-realized
+  [x on-success on-error]
+  (kd/on x on-success on-error))
+
+(defn claim! [d]
+  (kd/claim! d))
+
+(defn add-listener!
+  [d ls]
+  (md/add-listener! d ls))
+
+(defn cancel-listener! [d ls]
+  (md/cancel-listener! d ls))
+
+(defn listener [xf ef]
+  (md/listener xf ef))
+
+(defn error-deferred [x]
+  (kd/wrap-err x))
+
+(defn success-deferred [x]
+  (kd/wrap-val x))
+
+(defn deferred []
+  (kd/create))
+
+(defn error!
+  ([d x] (kd/error! d x))
+  ([d x t] (kd/error! d x t)))
+
+(defn success!
+  ([d x] (kd/success! d x))
+  ([d x t] (kd/success! d x t)))
+
+(defn deferred? [d]
+  (kd/deferred? d))
+
+
+(def ^:dynamic macroses-mode :knitty)
+
+(defn- kd-future-call [f]
+  (let [t (kd/create)]
+    (kd/on
+     (kd/future (kd/success! t (f)))
+     (fn [_])
+     (fn [e] (kd/error! t e)))
+    t))
+
+(defmacro future [& body]
+  `(case macroses-mode
+     :knitty   (#'kd-future-call (fn [] ~@body))
+     :manifold (md/future ~@body)))
+
+(defmacro loop [& rs]
+  `(case macroses-mode
+     :knitty (kd/loop ~@rs)
+     :manifold (md/loop ~@rs)))
+
+(defmacro recur [& rs]
+  `(case macroses-mode
+     :knitty (kd/recur ~@rs)
+     :manifold (md/recur ~@rs)))
+
+
+;; == knitty slow
+
+(defn- slow-kd-wrap-val [x]
+  (doto (kd/create)
+    (as-> d (future (Thread/sleep 20) (success! d x)))))
+
+(defn- slow-kd-wrap-err [x]
+  (doto (kd/create)
+   (as-> d (future (Thread/sleep 20) (error! d x)))))
+
+
+;; == modes
+
+(def mode-rebinds (atom {:knitty {}}))
+
+(defmacro reg-mode [name & {:as binds}]
+  `(swap! mode-rebinds assoc ~name ~binds))
+
+(defn with-modes-fixture []
+  (fn [t]
+    (doseq [[k bm] @mode-rebinds]
+        (testing (str "mode" k)
+          (with-redefs-fn bm t))
+      )))
+
+(def manifold-redefs
+  {#'chain            #'md/chain
+   #'catch            #'md/catch
+   #'chain'           #'md/chain'
+   #'catch'           #'md/catch'
+   #'finally          #'md/finally
+   #'->deferred       #'md/->deferred
+   #'connect          #'md/connect
+   #'alt              #'md/alt
+   #'zip              #'md/zip
+   #'on-realized      #'md/on-realized
+   #'claim!           #'md/claim!
+   #'add-listener!    #'md/add-listener!
+   #'cancel-listener! #'md/cancel-listener!
+   #'listener         #'md/listener
+   #'error-deferred   #'md/error-deferred
+   #'success-deferred #'md/success-deferred
+   #'deferred         #'md/deferred
+   #'error!           #'md/error!
+   #'success!         #'md/success!
+   #'deferred?        #'md/deferred?
+   #'macroses-mode    :manifold})
+
+(reg-mode
+ :manifold->knitty
+ #'success-deferred #'md/success-deferred
+ #'error-deferred   #'md/error-deferred
+ #'deferred         #'md/deferred)
+
+(reg-mode
+ :knitty-slow
+ #'success-deferred #'slow-kd-wrap-val
+ #'error-deferred   #'slow-kd-wrap-err)
+
+(reg-mode
+ :manifold
+ manifold-redefs)
+
+(reg-mode
+ :knitty->manifold
+ (->
+  manifold-redefs
+  (dissoc #'success-deferred
+          #'error-deferred
+          #'deferred
+          #'macroses-mode)))
+
+;; == quirks
+
+(defn fixup-promesa-print-method-test []
+  ;; fixture for 'promesa-print-method-test
+  (fn [t]
+    (let [mm ^clojure.lang.MultiFn print-method
+          m (-> print-method methods (get CompletionStage))]
+      (try
+        (doto mm
+          (.removeMethod CompletionStage)
+          (prefer-method CompletionStage IDeref))
+        (t)
+        (finally
+          (when m (.addMethod mm CompletionStage m)))))))
+
+
+(defn fixup-count-loggeg-errors []
+  (fn [t]
+    (log-test/with-log
+      (with-redefs [log-test/*stateful-log* log-test/*stateful-log*
+                    log/*logger-factory*    log/*logger-factory*]
+        (t)
+        (System/gc)
+        (Thread/sleep 50)
+        ))))
